ad8fafd02346b1321aa4cddd3cdc7ef6fad864f1
Doirena <barkauskaite.dovile@gmail.com>
2020-12-06 19:23:40 +0200

    Add select, from postgres db which is running by docker image

--- simple-rest-with-gradle/src/main/java/com/dovile/simplerestwithgradle/dao/PersonDataAccessService.java
+++ simple-rest-with-gradle/src/main/java/com/dovile/simplerestwithgradle/dao/PersonDataAccessService.java
@@ -2,14 +2,19 @@ package com.dovile.simplerestwithgradle.dao;
 
 import com.dovile.simplerestwithgradle.exception.ResourceNotFoundException;
 import com.dovile.simplerestwithgradle.model.Person;
+import lombok.AllArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@AllArgsConstructor
 @Repository("postgres")
 public class PersonDataAccessService implements PersonDao {
+
+    private final JdbcTemplate jdbcTemplate;
     @Override
     public int insertPerson(UUID id, Person person) {
         return 0;
@@ -17,12 +22,24 @@ public class PersonDataAccessService implements PersonDao {
 
     @Override
     public List<Person> getAllPerson() {
-        return List.of(new Person(UUID.randomUUID(), "FROM POSTGRES DB"));
+        final String sql = "SELECT id, name FROM person";
+        return jdbcTemplate.query(sql, (resultSet, i) ->{
+            UUID id = UUID.fromString(resultSet.getString("id"));
+            String name = resultSet.getString("name");
+            return new Person(id, name);
+        });
     }
 
     @Override
     public Optional<Person> selectPersonById(UUID id) {
-        return Optional.empty();
+        final String sql = "SELECT id, name FROM person WHERE id = ? ";
+        Person person = jdbcTemplate.queryForObject(sql, new Object[]{id},
+                (resultSet, i) ->{
+                    UUID personId = UUID.fromString(resultSet.getString("id"));
+                    String name = resultSet.getString("name");
+                    return new Person(personId, name);
+                });
+        return Optional.ofNullable(person);
     }
 
     @Override
