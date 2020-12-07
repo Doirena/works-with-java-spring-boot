791d42a76206be13d559876496936bc69471d398
Doirena <barkauskaite.dovile@gmail.com>
2020-12-07 15:55:47 +0200

    add Bifunction which has two arguments

--- javafuncional/src/main/java/com/dovile/javafuncional/functionalintarface/_Function.java
+++ javafuncional/src/main/java/com/dovile/javafuncional/functionalintarface/_Function.java
@@ -1,14 +1,15 @@
 package com.dovile.javafuncional.functionalintarface;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 /**
- *
  * @author Dovile Barkauskaite <barkauskaite.dovile@gmail.com>
  */
 public class _Function {
 
     public static void main(String[] args) {
+        //Function takes 1 argument and produces 1 result
         int increment = incrementByOne(0);
         System.out.println("By one " + increment);
 
@@ -22,6 +23,10 @@ public class _Function {
         Function<Integer, Integer> addBy1AndThenMultiblyBy10
                 = incrementByOneFuntion.andThen(multiplyBy10Funtion);
         System.out.println(addBy1AndThenMultiblyBy10.apply(4));
+
+        //BiFunction takes 2 arguments and produces 1 result
+        System.out.println(incrementByOneAndMultiply(4, 100));
+        System.out.println(incrementByOneAndMultiplyFunction.apply(4, 100));
     }
 
     /**
@@ -34,11 +39,29 @@ public class _Function {
     static Function<Integer, Integer> multiplyBy10Funtion = number -> number * 10;
 
     /**
-     * simple funtion which take one int variable and return int
+     * simple function which takes one int variable and returns int
      * @param number
-     * @return number
+     * @return int number + 1
      */
     static int incrementByOne(int number) {
         return number + 1;
+    }
+
+    /**
+     * BiFunction<T, U, R> t- type of first insert variable, u- type of second variable, r-type of result
+     * this function is the same like simple method below: incrementByOneAndMultiply
+     */
+    static BiFunction<Integer, Integer, Integer> incrementByOneAndMultiplyFunction =
+            (numberToIncrementByOne, numberToMultiplyBy) ->
+                    (numberToIncrementByOne + 1) * numberToMultiplyBy;
+
+    /**
+     * simple function with two arguments which returns int
+     * @param number
+     * @param numToMultiplyBy
+     * @return int (number + 1) * numToMultiplyBy
+     */
+    static int incrementByOneAndMultiply(int number, int numToMultiplyBy) {
+        return (number + 1) * numToMultiplyBy;
     }
 }
