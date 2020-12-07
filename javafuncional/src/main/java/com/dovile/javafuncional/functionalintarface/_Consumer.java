6d99e461e8fbe77bf61927a4ae1ebee89f0e7ea7
Doirena <barkauskaite.dovile@gmail.com>
2020-12-07 16:59:16 +0200

    create function using BiConsumer<T, U> interface

--- javafuncional/src/main/java/com/dovile/javafuncional/functionalintarface/_Consumer.java
+++ javafuncional/src/main/java/com/dovile/javafuncional/functionalintarface/_Consumer.java
@@ -2,6 +2,7 @@ package com.dovile.javafuncional.functionalintarface;
 
 import lombok.AllArgsConstructor;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 /**
@@ -13,34 +14,58 @@ public class _Consumer {
         Customer maria = new Customer("Maria", "999999");
         //Normal java function
         greetCustomer(maria);
+        greetCustomerV2(maria, false);
 
         //Function interface Consumer
         greetCustomerConsumer.accept(maria);
+        greetCustomerConsumerV2.accept(maria, false);
     }
 
     /**
      * Function made using Function Consumer<T>, t-type of insert argument, interface
      */
     static Consumer<Customer> greetCustomerConsumer = customer ->
-            System.out.println("Hello  "+ customer.customerName
-                    +", thanks for registering phone number "
-                    +customer.customerPhoneNumber);
+            System.out.println("Hello  " + customer.customerName
+                    + ", thanks for registering phone number "
+                    + customer.customerPhoneNumber);
+
+    /**
+     * interface BiConsumer<T, U> has to parameters, t - type of first arguments
+     * u - second argument, in example we take boolean if true shows number otherwise hide numeber
+     * analogue function is greetCustomerV2
+     */
+    static BiConsumer<Customer, Boolean> greetCustomerConsumerV2 = (customer, showPhoneNumber) ->
+            System.out.println("Hello  " + customer.customerName
+                    + ", thanks for registering phone number "
+                    + (showPhoneNumber ? customer.customerPhoneNumber : "hide number"));
 
     /**
      * Simple java function
+     *
      * @param customer
      */
-    static void greetCustomer(Customer customer){
-        System.out.println("Hello  "+ customer.customerName
-                +", thanks for registering phone number "
-                +customer.customerPhoneNumber);
+    static void greetCustomer(Customer customer) {
+        System.out.println("Hello  " + customer.customerName
+                + ", thanks for registering phone number "
+                + customer.customerPhoneNumber);
+    }
+
+    /**
+     * Simple java function with two arguments
+     *
+     * @param customer
+     * @param showPhoneNumber
+     */
+    static void greetCustomerV2(Customer customer, boolean showPhoneNumber) {
+        System.out.println("Hello  " + customer.customerName
+                + ", thanks for registering phone number "
+                + (showPhoneNumber ? customer.customerPhoneNumber : "hide number"));
     }
 
     @AllArgsConstructor
-    static class Customer{
+    static class Customer {
         private final String customerName;
         private final String customerPhoneNumber;
     }
-
 
 }
