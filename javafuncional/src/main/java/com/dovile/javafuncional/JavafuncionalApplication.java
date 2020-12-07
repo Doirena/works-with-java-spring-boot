42992cfe804eca8bdc98a15da5826ca8e09c0f88
Doirena <barkauskaite.dovile@gmail.com>
2020-12-07 13:50:27 +0200

    Create the project

---
+++ javafuncional/src/main/java/com/dovile/javafuncional/JavafuncionalApplication.java
@@ -0,0 +1,13 @@
+package com.dovile.javafuncional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavafuncionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavafuncionalApplication.class, args);
+	}
+
+}
