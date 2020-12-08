79962119a912cb2ff12196145761de269ae16977
Doirena <barkauskaite.dovile@gmail.com>
2020-12-09 01:03:17 +0200

    Create project using r2db, webflux and java 11

---
+++ crudusingr2dbcwebflux/src/main/java/com/dovile/crudusingr2dbcwebflux/Crudusingr2dbcwebfluxApplication.java
@@ -0,0 +1,12 @@
+package com.dovile.crudusingr2dbcwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Crudusingr2dbcwebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Crudusingr2dbcwebfluxApplication.class, args);
+    }
+}
