bc245853b5d4aaab033aec1f55f05990bd8944d6
Doirena <barkauskaite.dovile@gmail.com>
2020-12-09 01:14:28 +0200

    correct Person class align and create Person repository using r2db

--- crudusingr2dbcwebflux/src/main/java/com/dovile/crudusingr2dbcwebflux/entity/Person.java
+++ crudusingr2dbcwebflux/src/main/java/com/dovile/crudusingr2dbcwebflux/entity/Person.java
@@ -1,6 +1,8 @@
 package com.dovile.crudusingr2dbcwebflux.entity;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
 
 /**
